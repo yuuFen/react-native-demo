e26a56f5399035275fadaf9c66846bd1c0fcd9e7
Yu <me@yuufen.com>
2020-07-20 20:25:59 +0800

    fetch / httpTools / API.js

--- src/Message/MessageScreen.js
+++ src/Message/MessageScreen.js
@@ -2,8 +2,52 @@ import React from 'react';
 import {Text, View, Button} from 'react-native';
 import {connect} from 'react-redux';
 import {updateMessage} from './action';
+import {APIS, APP} from '../constants/API';
+import {fetchGet} from '../util/httpTool';
 
 const MessageScreen = ({page, messageList, updateMessage}) => {
+  //http://m.app.haosou.com/index/getData?type=1&page=1
+  const fetchMessage = (curPage) => {
+    const requestParam = {
+      ...APP,
+      page: curPage,
+    };
+    fetchGet({
+      serviceType: APIS.ServiceInit,
+      params: requestParam,
+      success: (response) => {
+        if (curPage === 1) {
+          // const {setMessage} = this.props;
+          // setMessage({
+          //   list: response.list,
+          //   page: curPage,
+          // });
+          // this.setState({
+          //   isLoading: false,
+          //   // messageList: response.list,
+          //   isRefesh: false,
+          //   // page: curPage
+          // });
+        } else {
+          // const {updateMessageList} = this.props;
+          // updateMessageList({
+          //   page: curPage,
+          //   list: this.props.messageList.concat(response.list),
+          // });
+          // this.setState({
+          //   isLoading: false,
+          //   // page: curPage,
+          //   showFoot: 0,
+          //   // messageList: this.state.messageList.concat(response.list)
+          // });
+        }
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  };
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>
---
+++ src/constants/API.js
@@ -0,0 +1,10 @@
+export const APIS = {
+  Main_Path: 'http://m.app.haosou.com/index',
+
+  ServiceInit: '/getData',
+  //.....
+}
+
+export const APP = {
+  type: 1,
+};
